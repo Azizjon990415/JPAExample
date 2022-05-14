28fa6e97e0fb248f43598a65dffaa69b38a09594
@Azizjon <azizjon990415@gmail.com>
2022-05-14 10:38:56 +0500

    Rest with JPA

--- src/main/java/uz/lab/jpaexample/controller/BookController.java
+++ src/main/java/uz/lab/jpaexample/controller/BookController.java
@@ -1,19 +1,41 @@
 package uz.lab.jpaexample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.lab.jpaexample.DTO.BookDTO;
 import uz.lab.jpaexample.service.BookService;
+
+import java.util.List;
 
 @RestController
 public class BookController {
     @Autowired
     BookService bookService;
 
+    @GetMapping("api/book")
+    public List<BookDTO> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("api/book/{id}")
+    public BookDTO getBook(@PathVariable Long id){
+        return bookService.getBook(id);
+    }
+
     @PostMapping("api/book")
     public BookDTO saveNewBook(@RequestBody BookDTO bookDTO){
         return bookService.saveBook(bookDTO);
     }
+
+    @PutMapping("api/book/{id}")
+    public BookDTO editBook(@PathVariable Long id,@RequestBody BookDTO bookDTO){
+        return bookService.editBook(id,bookDTO);
+    }
+
+    @DeleteMapping("api/book/{id}")
+    public String deleteBook(@PathVariable Long id){
+        return bookService.deleteBook(id);
+    }
 }
+
+
--- src/main/java/uz/lab/jpaexample/service/BookService.java
+++ src/main/java/uz/lab/jpaexample/service/BookService.java
@@ -7,6 +7,10 @@ import uz.lab.jpaexample.domain.Book;
 import uz.lab.jpaexample.mapper.BookMapper;
 import uz.lab.jpaexample.repository.BookRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BookService {
     @Autowired
@@ -14,9 +18,42 @@ public class BookService {
     @Autowired
     BookMapper bookMapper;
 
+    public List<BookDTO> getAllBooks() {
+        List<Book> allBooks = bookRepository.findAll();
+        List<BookDTO> bookDTOList = new ArrayList<>();
+        allBooks.forEach(book -> {
+            bookDTOList.add(bookMapper.mapDomaintoDTO(book));
+        });
+        return bookDTOList;
+    }
+
+    public BookDTO getBook(Long id) {
+        Optional<Book> byId = bookRepository.findById(id);
+        if (byId.isPresent()){
+            BookDTO bookDTO = bookMapper.mapDomaintoDTO(byId.get());
+            return bookDTO;
+        } else {
+            throw new NullPointerException("Book not found!");
+        }
+    }
+
     public BookDTO saveBook(BookDTO bookDTO){
         Book savedBook = bookRepository.save(bookMapper.mapDTOtoDomain(bookDTO));
         BookDTO savedBookDTO = bookMapper.mapDomaintoDTO(savedBook);
         return savedBookDTO;
+    }
+
+
+    public BookDTO editBook(Long id, BookDTO bookDTO) {
+        Book book = bookMapper.mapDTOtoDomain(bookDTO);
+        book.setId(id);
+        Book savedBook = bookRepository.save(book);
+        BookDTO savedBookDTO = bookMapper.mapDomaintoDTO(savedBook);
+        return savedBookDTO;
+    }
+
+    public String deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return "Book deleted";
     }
 }
